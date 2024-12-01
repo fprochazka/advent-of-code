dbd1f63e3cdf1cf7e02a333fcbc3bd67aa32b3f1
Filip Prochazka <dev@fprochazka.cz>
2024-12-01 16:30:38 +0100

    init

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin
+    kotlin("jvm") version "2.1.0"
+}
+
+kotlin {
+    // Use a specific Java version to make it easier to work in different environments.
+    jvmToolchain(23)
+}
+
+dependencies {
+
+}
+
+tasks.withType<Test>().configureEach {
+    // Configure all test Gradle tasks to use JUnitPlatform.
+    useJUnitPlatform()
+
+    // Log information about all test results, not only the failed ones.
+    testLogging {
+        events(
+            TestLogEvent.FAILED,
+            TestLogEvent.PASSED,
+            TestLogEvent.SKIPPED
+        )
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+dependencyResolutionManagement {
+    // Use Maven Central as the default repository (where Gradle will download dependencies) in all subprojects.
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "advent-of-code"
