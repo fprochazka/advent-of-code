ef45067be2ec167c02f4acc5b054769f346dfeda
Filip Prochazka <dev@fprochazka.cz>
2024-12-10 20:03:45 +0100

    aoc2024 - day 9 - optimize

--- README.md
+++ README.md
@@ -4,30 +4,30 @@ Snapshot of `aoc.measure.Aoc2024` output:
 
 ```
  day  task                   [average]          [min]          [max]
-  1.    1.       input:        37.29ms        37.29ms        37.29ms
-  1.    2.       input:         6.92ms         6.92ms         6.92ms
-  2.    1.       input:         8.48ms         8.48ms         8.48ms
-  2.    2.       input:        15.35ms        15.35ms        15.35ms
-  3.    1.       input:        24.10ms        24.10ms        24.10ms
-  3.    2.       input:         5.41ms         5.41ms         5.41ms
-  4.    1.       input:       137.29ms       137.29ms       137.29ms
-  4.    2.       input:        29.96ms        29.96ms        29.96ms
-  5.    1.       input:        14.53ms        14.53ms        14.53ms
-  5.    2.       input:        10.84ms        10.84ms        10.84ms
-  6.    1.       input:        20.05ms        20.05ms        20.05ms
-  6.    2.       input:      2286.65ms      2286.65ms      2286.65ms
-  7.    1.       input:       214.60ms       214.60ms       214.60ms
-  7.    2.       input:      8599.53ms      8599.53ms      8599.53ms
-  8.    1.       input:         9.93ms         9.93ms         9.93ms
-  8.    2.       input:         4.76ms         4.76ms         4.76ms
-  9.    1.       input:        28.72ms        28.72ms        28.72ms
-  9.    1. perf_test_1:        14.98ms        14.98ms        14.98ms
-  9.    1. perf_test_2:        22.83ms        22.83ms        22.83ms
-  9.    2.       input:       131.47ms       131.47ms       131.47ms
-  9.    2. perf_test_1:      2740.53ms      2740.53ms      2740.53ms
-  9.    2. perf_test_2:        TIMEOUT        TIMEOUT        TIMEOUT
- 10.    1.       input:        10.67ms        10.67ms        10.67ms
- 10.    2.       input:         6.09ms         6.09ms         6.09ms
+  1.    1.       input:        39.35ms        39.35ms        39.35ms
+  1.    2.       input:         8.64ms         8.64ms         8.64ms
+  2.    1.       input:        10.21ms        10.21ms        10.21ms
+  2.    2.       input:        13.72ms        13.72ms        13.72ms
+  3.    1.       input:        22.68ms        22.68ms        22.68ms
+  3.    2.       input:         8.06ms         8.06ms         8.06ms
+  4.    1.       input:       124.65ms       124.65ms       124.65ms
+  4.    2.       input:        19.76ms        19.76ms        19.76ms
+  5.    1.       input:        10.11ms        10.11ms        10.11ms
+  5.    2.       input:         9.91ms         9.91ms         9.91ms
+  6.    1.       input:        17.91ms        17.91ms        17.91ms
+  6.    2.       input:      2399.79ms      2399.79ms      2399.79ms
+  7.    1.       input:       213.67ms       213.67ms       213.67ms
+  7.    2.       input:      8583.49ms      8583.49ms      8583.49ms
+  8.    1.       input:        10.19ms        10.19ms        10.19ms
+  8.    2.       input:         5.33ms         5.33ms         5.33ms
+  9.    1.       input:        25.96ms        25.96ms        25.96ms
+  9.    1. perf_test_1:        10.91ms        10.91ms        10.91ms
+  9.    1. perf_test_2:        28.07ms        28.07ms        28.07ms
+  9.    2.       input:        58.45ms        58.45ms        58.45ms
+  9.    2. perf_test_1:        58.91ms        58.91ms        58.91ms
+  9.    2. perf_test_2:       201.36ms       201.36ms       201.36ms
+ 10.    1.       input:        12.29ms        12.29ms        12.29ms
+ 10.    2.       input:         5.78ms         5.78ms         5.78ms
 
-Sum of min for normal inputs: 11.602638s
+Sum of min for normal inputs: 11.599925s
 ```
--- src/main/kotlin/aoc/y2024/Aoc2024Day09.kt
+++ src/main/kotlin/aoc/y2024/Aoc2024Day09.kt
@@ -1,10 +1,13 @@
 package aoc.y2024
 
 import utils.Resource
+import java.util.*
 
 fun main() {
     solve(Resource.named("aoc2024/day09/example1.txt"))
     solve(Resource.named("aoc2024/day09/input.txt"))
+    solve(Resource.named("aoc2024/day09/perf_test_1.txt"))
+    solve(Resource.named("aoc2024/day09/perf_test_2.txt"))
 }
 
 private fun solve(input: Resource) {
@@ -22,52 +25,21 @@ fun Resource.day09(): Day09 = Day09(
 
 data class Day09(val diskMap: String) {
 
-    val unpacked by lazy {
-        unpack(diskMap)
-    }
-
     val result1 by lazy {
-        fragmentByFileSegments(unpacked.first)
+        fragmentByFileSegments()
             .mapIndexed { position, fileId -> position * fileId }
             .sum()
     }
 
     val result2 by lazy {
-        moveWholeFiles(unpacked.first, unpacked.second)
+        moveWholeFiles()
             .mapIndexed { position, fileId -> position * (fileId ?: 0) }
             .sum()
     }
 
-    /**
-     * left => [list of fileIds, indexes are positions]
-     * right => [fileId => length to position]
-     */
-    fun unpack(data: String): Pair<List<Long?>, Map<Long, Pair<Int, Int>>> {
-        val layout = mutableListOf<Long?>()
-        val metadata = mutableMapOf<Long, Pair<Int, Int>>()
+    fun fragmentByFileSegments(): List<Long> = buildList {
+        val layout = diskContentsByPosition()
 
-        var fileId = 0L
-        for ((isFileBlock, blockSize) in data.asSequence().map { it.digitToInt() }.mapIndexed { index, value -> (index.mod(2) == 0) to value }) {
-            if (isFileBlock) {
-                metadata[fileId] = blockSize to layout.size
-
-                repeat(blockSize) {
-                    layout.add(fileId)
-                }
-
-                fileId++
-
-            } else {
-                repeat(blockSize) {
-                    layout.add(null)
-                }
-            }
-        }
-
-        return layout to metadata
-    }
-
-    fun fragmentByFileSegments(layout: List<Long?>): List<Long> = buildList {
         val fromStart = layout.indices.iterator()
         val fromEnd = layout.indices.reversed().iterator()
 
@@ -90,62 +62,165 @@ data class Day09(val diskMap: String) {
         }
     }
 
-    fun moveWholeFiles(layout: List<Long?>, metadata: Map<Long, Pair<Int, Int>>): List<Long?> {
-        // [position => fileId]
-        val result = layout.toMutableList()
+    fun diskContentsByPosition(): MutableList<Long?> {
+        val layout = mutableListOf<Long?>()
 
-        // [fileId => length to position]
-        val fileMetadataByMaxId = metadata.toSortedMap(Comparator.naturalOrder<Long>().reversed())
-
-        var leftMostGap = 0
-        for ((fileId, fileMetadata) in fileMetadataByMaxId.entries) {
-            val (fileSize, originalFilePosition) = fileMetadata
-
-            // leftmost gap can only move right
-            leftMostGap = (leftMostGap until result.size).firstOrNull { result[it] == null } ?: leftMostGap
-
-            if (leftMostGap > originalFilePosition) {
-                break // cannot move files before the leftmost gap
-            }
-
-            for (gap in result.gapsSequence(leftMostGap, originalFilePosition)) {
-                if (fileSize > gap.length()) {
-                    continue
+        var fileId = 0L
+        for ((isFile, blockSize) in diskMapBlocks()) {
+            if (isFile) {
+                repeat(blockSize) {
+                    layout.add(fileId)
                 }
+                fileId++
 
-                for (i in 0 until fileSize) {
-                    result[gap.start + i] = fileId
-                    result[originalFilePosition + i] = null
+            } else {
+                repeat(blockSize) {
+                    layout.add(null)
                 }
-
-                break; // move done, attempt next file
             }
         }
-
-        return result
+        return layout
     }
 
-    /**
-     * Creates a lazy sequence of gap index ranges in O(n)
-     */
-    fun List<Long?>.gapsSequence(fromIndex: Int, toIndex: Int): Sequence<IntRange> {
-        val list = this
-        return sequence {
-            val iterator = (fromIndex..toIndex).iterator()
-            for (gapStartIncl in iterator) {
-                if (list[gapStartIncl] != null) continue
+    fun diskMapBlocks(): Sequence<DiskMapBlock> =
+        diskMap.asSequence()
+            .map { it.digitToInt() }
+            .mapIndexed { index, value -> DiskMapBlock((index.mod(2) == 0), value) }
 
-                var gapEndExcl = gapStartIncl + 1;
-                for (j in iterator) {
-                    gapEndExcl = j
-                    if (list[j] != null) break
+    data class DiskMapBlock(val isFile: Boolean, val blockSize: Int)
+
+    fun moveWholeFiles(): List<Long?> {
+        data class File(
+            val id: Long,
+            val size: Int,
+            val originalPosition: Int,
+        )
+
+        class State(
+            // [position => fileId]
+            val result: MutableList<Long?>,
+            // [fileId => file]
+            val fileMetadataByMaxId: SortedMap<Long, File>,
+        ) {
+
+            val gaps: Sequence<IntRange>
+                get() = sequence {
+                    val iterator = result.indices.iterator()
+                    for (gapStartIncl in iterator) {
+                        if (result[gapStartIncl] != null) continue
+
+                        var gapEndExcl = gapStartIncl + 1;
+                        for (j in iterator) {
+                            gapEndExcl = j
+                            if (result[j] != null) break
+                        }
+
+                        yield(gapStartIncl until gapEndExcl)
+                    }
                 }
 
-                yield(gapStartIncl until gapEndExcl)
+            // [size => [gaps]]
+            val leftMostGapsIndex: MutableMap<Int, SortedMap<Int, IntRange>> by lazy {
+                val result = mutableMapOf<Int, SortedMap<Int, IntRange>>()
+
+                for (gap in gaps) {
+                    for (size in gap.fittingSizes()) {
+                        result
+                            .getOrPut(size) { sortedMapOf(Comparator.naturalOrder<Int>()) }
+                            .put(gap.start, gap)
+                    }
+                }
+
+                return@lazy result
+            }
+
+            val filesToMove: Sequence<File> =
+                fileMetadataByMaxId.values.asSequence()
+                    .takeWhile { file -> (leftMostGap?.start ?: 0) < file.originalPosition }
+
+            val leftMostGap: IntRange?
+                get() = getLeftMostGapForSize(1)
+
+            fun getLeftMostGapForSize(size: Int): IntRange? =
+                leftMostGapsIndex[size]?.firstEntry()?.value
+
+            fun tryMoveToGapClosestToDiskStart(file: File) {
+                findGapForFile(file)?.let { gap ->
+                    moveFileToGap(file, gap)
+                }
+            }
+
+            fun findGapForFile(file: File): IntRange? =
+                getLeftMostGapForSize(file.size)
+                    ?.takeIf { it.start <= file.originalPosition }
+
+            fun moveFileToGap(file: File, gap: IntRange) {
+                require(file.size <= gap.length()) { "File must fit into the gap" }
+
+                for (i in 0 until file.size) {
+                    result[gap.start + i] = result[file.originalPosition + i]
+                    result[file.originalPosition + i] = null
+                }
+
+                reindexGap(gap, file.size)
+            }
+
+            fun reindexGap(gap: IntRange, shorterBy: Int) {
+                // remove the gap from the index
+                for (size in gap.fittingSizes()) {
+                    leftMostGapsIndex[size]!!.remove(gap.start)
+                }
+
+                val newStartIndex = gap.start + shorterBy
+                val newGap = newStartIndex..gap.endInclusive
+
+                // reindex the new gap
+                for (size in newGap.fittingSizes()) {
+                    leftMostGapsIndex[size]!!.put(newGap.start, newGap)
+                }
+            }
+
+            override fun toString(): String =
+                result
+                    .mapIndexed { index, value -> "$index: " + (value?.toString() ?: ".") }
+                    .joinToString("\n")
+
+            fun IntRange.fittingSizes(): List<Int> = if (this.length() > 0) toList().indices.map { it + 1 } else emptyList()
+
+        }
+
+        val layout = mutableListOf<Long?>()
+        val metadata = sortedMapOf<Long, File>(Comparator.naturalOrder<Long>().reversed())
+
+        var fileId = 0L
+        for ((isFile, blockSize) in diskMapBlocks()) {
+            if (isFile) {
+                metadata[fileId] = File(fileId, blockSize, layout.size)
+
+                repeat(blockSize) {
+                    layout.add(fileId)
+                }
+                fileId++
+
+            } else {
+                repeat(blockSize) {
+                    layout.add(null)
+                }
             }
         }
+
+        val state = State(layout, metadata)
+        state.filesToMove.forEach { file ->
+            state.tryMoveToGapClosestToDiskStart(file)
+        }
+
+        return state.result
     }
 
     fun IntRange.length() = last - first + 1
+
+    fun <T> Sequence<T>.withLongIndex(): Sequence<Pair<Long, T>> {
+        return this.withIndex().map { (index, value) -> index.toLong() to value }
+    }
 
 }
