ae3dcebfcbef4e9f950453cfe23b93b12390e4fe
Filip Procházka <dev@fprochazka.cz>
2024-12-21 06:00:27 +0100

    aoc2024 - day 21 - template

---
+++ src/main/kotlin/aoc/y2024/Aoc2024Day21.kt
@@ -0,0 +1,14 @@
+package aoc.y2024
+
+import aoc.utils.Resource
+
+fun Resource.day21(): Day21 = Day21(
+    content()
+)
+
+data class Day21(val data: String) {
+
+    val result1: String by lazy { TODO() }
+    val result2: String by lazy { TODO() }
+
+}
